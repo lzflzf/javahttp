9daedeebe7e39e6082cbd15cf5d5c41efa45d434
lizhifeng <lzfqidi@gmail.com>
2018-03-14 12:11:03 +0800

    增加黑名单

--- src/main/java/com/lizhifeng/study/nb/HandleThread1.java
+++ src/main/java/com/lizhifeng/study/nb/HandleThread1.java
@@ -18,10 +18,9 @@ public class HandleThread1 implements Runnable {
 
     @Override
     public void run() {
+        System.out.println(Thread.currentThread());
+
         try {
-
-            System.out.println(Thread.currentThread());
-
             InputStream inStream = incoming.getInputStream();
             OutputStream outStream = incoming.getOutputStream();
             PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
@@ -30,72 +29,67 @@ public class HandleThread1 implements Runnable {
             String filePath = "";
             String ContentType = "";
 
-            try (Scanner in = new Scanner(inStream)) {
-                while (in.hasNextLine()) {
-                    String line = in.nextLine();
-                    System.out.println(line);
-                    if (!flag) {
-                        String[] MethonPathHttpversion = line.split(" ");
-                        filePath = MethonPathHttpversion[1];
+            Scanner in = new Scanner(inStream);
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                System.out.println(line);
+                if (!flag) {
+                    String[] MethonPathHttpversion = line.split(" ");
+                    filePath = MethonPathHttpversion[1];
 
-                        filePath = URLDecoder.decode(filePath,"UTF-8") ;
+                    filePath = URLDecoder.decode(filePath, "UTF-8");
 
-                        if (MethonPathHttpversion[1].equals("/")) {
-                            filePath = index;
-                        }
-
-                        if (filePath.indexOf('?') > 0) {
-                            filePath = filePath.substring(0, filePath.indexOf('?'));
-                        }
-
-                        ContentType = MimeTypes.getMimeType(filePath) ;
-
-                        flag = true;    //  解析第一行
-                        continue;
+                    if (MethonPathHttpversion[1].equals("/")) {
+                        filePath = index;
                     }
 
-                    if (line.equals("")) {
-                        break;
+                    if (filePath.indexOf('?') > 0) {
+                        filePath = filePath.substring(0, filePath.indexOf('?'));
                     }
+
+                    ContentType = MimeTypes.getMimeType(filePath);
+
+                    flag = true;    //  解析第一行
+                    continue;
                 }
 
-
-                filePath = rootPath + filePath;
-                File file = new File(filePath);
-                if (file.exists()) {
-                    out.println("HTTP/1.1 200 OK");
-                    out.println("Content-Type: " + ContentType + "; charset=utf-8");
-                    // out.println("Content-Encoding: gzip");  // 正文使用gzip进行压缩
-                    out.println();    //  输出header头
-
-                    FileInputStream filein = new FileInputStream(file);
-                    byte[] bytes = new byte[8192];
-                    int total = filein.read(bytes);
-                    while (total != -1) {
-                        outStream.write(bytes, 0, total);
-                        outStream.flush();
-                        total = filein.read(bytes);
-                    }
-                } else {
-                    out.println("HTTP/1.1 404 Not Found");
-                    // out.println("Content-Type: " + ContentType + "; charset=utf-8");
-                    out.println();
+                if (line.equals("")) {
+                    break;
                 }
-
-                out.flush();
-                outStream.close();
-                out.close();
-                incoming.close();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+
+            String fullFilePath = rootPath + filePath;
+            File file = new File(fullFilePath);
+
+            if (file.exists() && file.isFile()) {
+                out.println("HTTP/1.1 200 OK");
+                out.println("Content-Type: " + ContentType + "; charset=utf-8");
+                // out.println("Content-Encoding: gzip");  // 正文使用gzip进行压缩
+                out.println();    //  输出header头
+
+                FileInputStream filein = new FileInputStream(file);
+                byte[] bytes = new byte[8192];
+                int total = filein.read(bytes);
+                while (total != -1) {
+                    outStream.write(bytes, 0, total);
+                    outStream.flush();
+                    total = filein.read(bytes);
+                }
+            } else {
+                out.println("HTTP/1.1 404 Not Found");
+                // out.println("Content-Type: " + ContentType + "; charset=utf-8");
+                out.println();
+            }
+
+            out.flush();
+            outStream.close();
+            out.close();
+            incoming.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    // 如何从请求报文中获取参数并且调用相应的servlet 处理逻辑
-    // jsp 还是编译成servlet
-    // 如何支持多host和多webapp
 }
+
+
 
--- src/main/java/com/lizhifeng/study/nb/Server.java
+++ src/main/java/com/lizhifeng/study/nb/Server.java
@@ -14,12 +14,20 @@ public class Server {
             while (true) {
                 // wait for client connection
                 Socket incoming = s.accept();
-
+//                System.out.println(incoming.getRemoteSocketAddress().toString()) ;
+//                System.out.println(incoming.getInetAddress().getHostAddress());
+//                System.out.println(incoming.getInetAddress().getHostName());
                 new Thread(new HandleThread1(incoming)).start();
 
             }
         }
     }
 }
+
+// 如何从请求报文中获取参数并且调用相应的servlet 处理逻辑
+// jsp 还是编译成servlet
+// 如何支持多host和多webapp
+// NIO
+
 
 
